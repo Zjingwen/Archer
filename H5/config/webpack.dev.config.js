3412b4398514eb24b77be8178a8e40c2fdd7afab
静文（jingwen） <jingwen@xiaoshijie.com>
2018-07-12 23:23:40 +0800

    release * change

--- H5/config/webpack.dev.config.js
+++ H5/config/webpack.dev.config.js
@@ -1,25 +1,19 @@
 const path = require('path');
-const index = path.resolve(__dirname,'../src/pages/index/index.js');
-const item = path.resolve(__dirname,'../src/pages/item/item.js');
-const outPath = path.resolve(__dirname,'../dist/');
-
-const HtmlWebpackPlugin = require('html-webpack-plugin');// 生成html
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');// 抽离 css
 
-// const util = require('./util');
-// const entryPaths = util.entries();
-// const htmlPlugin = util.htmlPlugin();
+const util = require('./util');
+const entryPaths = util.entries();
+const Plugins = util.htmlPlugin('h5');
+
+Plugins.push(
+  new MiniCssExtractPlugin({
+    filename: "[name].css",
+  }),
+);
 
 module.exports = {
   mode: 'development',
-  entry: {
-    index: index,
-    item: item,
-  },
-  output:{
-    path: outPath,
-    filename: '[name].js',
-  },
+  entry: entryPaths,
   module:{
     rules:[
       {
@@ -35,33 +29,9 @@ module.exports = {
     compress: true,
     inline: true,
     open: true,
-    port: 9000
+    port: 9000,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname,'../src/pages/index/index.html'),
-      filename: 'h5/index',
-      chunks: [ 'index' ],
-      inject: true
-    }),
-    // new MiniCssExtractPlugin({
-    //   filename: path.resolve(__dirname,'../src/pages/*/**.css'),
-    //   chunkFilename: '[name].css',
-    // }),
-    // new MiniCssExtractPlugin({
-    //   filename: path.resolve(__dirname,'../pages/item/item.css'),
-    //   chunkFilename: "item.css"
-    // }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname,'../src/pages/item/item.html'),
-      filename: 'h5/item',
-      chunks: [ 'item' ],
-      inject: true
-    }),
-  ],
+  plugins: Plugins,
   resolve:{
     extensions: ['*', '.js', '.css'],
     alias:{
