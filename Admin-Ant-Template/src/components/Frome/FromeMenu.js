0db7886bd51213987fd862051256ee6a1f1798ae
静文（jingwen） <jingwen@xiaoshijie.com>
2019-04-04 00:33:50 +0800

    feature: 抽离FromeHeader、FromeMenu文件

---
+++ Admin-Ant-Template/src/components/Frome/FromeMenu.js
@@ -0,0 +1,83 @@
+/**
+ * @todo 优化 补充注释
+ */
+import React from 'react';
+import { Layout, Menu, Icon } from 'antd';
+import { Link } from 'dva/router';
+import routerMap from '@pages';
+import {jsonToQuery} from '@utils/assist';
+
+const { Sider } = Layout;
+const SubMenu = Menu.SubMenu;
+
+class FromeMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    /**
+     * @todo 使用router的方法，不用window方法
+     */
+    const locationHash = window.location.hash.indexOf('#') > -1 ? window.location.hash.slice(1) : window.location.pathname;
+    this.state = {
+      key: [locationHash]
+    };
+  }
+
+  subMenuChildrens({next, path}){
+    return next.map((value)=>{
+      const QUERY = value.hasOwnProperty('query') ? jsonToQuery(value.query) : '';
+      const PATH = path + value.path;
+
+      return (<Menu.Item key={PATH}>
+        <Link to={{
+          pathname: PATH,
+          search: `?${QUERY}`
+        }}>
+          <Icon type={value.iconType} />{value.breadcrumbName}
+        </Link>
+      </Menu.Item>);
+    });
+  }
+
+
+  render() {
+    const ROUTER_SELECT = routerMap.filter((v) => {
+      if(v.hasOwnProperty('menu')) return v.menu ? true : false;
+      return true;
+    });
+
+    const MENU_ITEMS = ROUTER_SELECT.map((value) => {
+      if(value.hasOwnProperty('next')){
+        return(<SubMenu key={value.path} title={<><Icon type={value.iconType} />{value.breadcrumbName}</>}>
+          {this.subMenuChildrens({
+            next: value.next,
+            path: value.path,
+          })}
+        </SubMenu>);
+      };
+
+      return (<Menu.Item key={value.path}>
+        <Link to={value.path}>
+          <Icon type={value.iconType} />{value.breadcrumbName}
+        </Link>
+      </Menu.Item>);
+    });
+
+    return (
+      <Sider
+        trigger={null}
+        collapsible
+        collapsed={this.props.collapsed}
+      >
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={this.state.key}
+        >
+          {MENU_ITEMS}
+        </Menu>
+      </Sider>
+    );
+  };
+};
+
+export default FromeMenu;
--- Admin-Ant-Template/src/components/Frome/index.js
+++ Admin-Ant-Template/src/components/Frome/index.js
@@ -1,115 +1,16 @@
 /**
  * @todo bug 会重复渲染两次
- * @todo 优化 抽离FromeMenu、FromeHeader为独立文件
  * @todo 优化 menu有子目录的时候，点击子目录不关闭
+ * @todo 优化 是否可以改造为方法函数
  */
 import styles from './index.css';
 import React from 'react';
-import { Layout, Menu, Icon, Dropdown } from 'antd';
-import { Link } from 'dva/router';
-import routerMap from '@pages';
-import {jsonToQuery} from '@utils/assist';
+import { Layout } from 'antd';
 
-const { Header, Sider, Content } = Layout;
-const SubMenu = Menu.SubMenu;
+import FromeMenu from './FromeMenu';
+import FromeHeader from './FromeHeader';
 
-class FromeMenu extends React.Component {
-  constructor(props) {
-    super(props);
-    /**
-     * @todo 使用router的方法，不用window方法
-     */
-    const locationHash = window.location.hash.indexOf('#') > -1 ? window.location.hash.slice(1) : window.location.pathname;
-    this.state = {
-      key: [locationHash]
-    };
-  }
-
-  subMenuChildrens({next, path}){
-    return next.map((value)=>{
-      const QUERY = value.hasOwnProperty('query') ? jsonToQuery(value.query) : '';
-      const PATH = path + value.path;
-
-      return (<Menu.Item key={PATH}>
-        <Link to={{
-          pathname: PATH,
-          search: `?${QUERY}`
-        }}>
-          <Icon type={value.iconType} />{value.breadcrumbName}
-        </Link>
-      </Menu.Item>);
-    });
-  }
-
-
-  render() {
-    const ROUTER_SELECT = routerMap.filter((v) => {
-      if(v.hasOwnProperty('menu')) return v.menu ? true : false;
-      return true;
-    });
-
-    const MENU_ITEMS = ROUTER_SELECT.map((value) => {
-      if(value.hasOwnProperty('next')){
-        return(<SubMenu key={value.path} title={<><Icon type={value.iconType} />{value.breadcrumbName}</>}>
-          {this.subMenuChildrens({
-            next: value.next,
-            path: value.path,
-          })}
-        </SubMenu>);
-      };
-
-      return (<Menu.Item key={value.path}>
-        <Link to={value.path}>
-          <Icon type={value.iconType} />{value.breadcrumbName}
-        </Link>
-      </Menu.Item>);
-    });
-
-    return (
-      <Sider
-        trigger={null}
-        collapsible
-        collapsed={this.props.collapsed}
-      >
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={this.state.key}
-        >
-          {MENU_ITEMS}
-        </Menu>
-      </Sider>
-    );
-  };
-};
-
-class FromeHeader extends React.Component {
-
-  render() {
-    const menu = (
-      <Menu>
-        <Menu.Item>
-          <span>退出登陆</span>
-        </Menu.Item>
-      </Menu>
-    );
-
-    return (
-      <Header className={styles.header}>
-        <Icon
-          className={styles.trigger}
-          type={this.props.collapsed ? 'menu-unfold' : 'menu-fold'}
-          onClick={this.props.updateStateProp}
-        />
-        <div style={{ float: "right" }}>
-          <Dropdown overlay={menu}>
-            <span>静文</span>
-          </Dropdown>
-        </div>
-      </Header>
-    );
-  }
-};
+const { Content } = Layout;
 
 class Frome extends React.Component {
   state = {
@@ -122,13 +23,12 @@ class Frome extends React.Component {
     });
   };
 
-
   render() {
     return (
       <Layout className={styles.layout}>
-        <FromeMenu collapsed={this.state.collapsed}></FromeMenu>
+        <FromeMenu collapsed={this.state.collapsed} />
         <Layout>
-          <FromeHeader collapsed={this.state.collapsed} updateStateProp={this.toggle}></FromeHeader>
+          <FromeHeader collapsed={this.state.collapsed} updateStateProp={this.toggle} />
           <Content className={styles.content}>
             {this.props.children}
           </Content>
