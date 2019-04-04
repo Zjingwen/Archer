ed218c627cbafabb621e893e2398472a089ac9f4
静文（jingwen） <jingwen@xiaoshijie.com>
2019-04-04 10:52:16 +0800

    feature: 补充todo

--- Admin-Ant-Template/src/components/Frome/FromeHeader.js
+++ Admin-Ant-Template/src/components/Frome/FromeHeader.js
@@ -1,3 +1,7 @@
+/**
+ * @todo 优化 是否可以改造为方法函数
+ * @todo 优化，用row和col来代替index.css
+ */
 import styles from './index.css';
 import React from 'react';
 import { Layout, Menu, Icon, Dropdown } from 'antd';
