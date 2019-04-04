8588adf171ae4366edbe2da9a2d5d3bb1f9ca15e
静文（jingwen） <jingwen@xiaoshijie.com>
2019-04-04 14:47:23 +0800

    feature: 完善login的mock测试数据

--- Admin-Ant-Template/src/pages/userPage/sign/index.js
+++ Admin-Ant-Template/src/pages/userPage/sign/index.js
@@ -6,33 +6,15 @@ import axios from 'axios';
 
 const FormItem = Form.Item;
 
-
-class Logo extends React.Component {
-  render() {
-    return (
-      <p style={{ textAlign: "center" }}>管理台</p>
-    );
-  }
-}
-
-class LoginForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      disabled: false,
-      getCodeBtnText: '获取验证码',
-      sleep: 15
-    };
-  }
-
+class Login extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
         const params = { ...values };
-        axios.get('/api/admin/user/login', { params: params }).then(res => {
+        axios.get('/api/user/sign', { params: params }).then(res => {
           const json = res.data;
-          const msg = json.result.msg;
+          const msg = json.status.msg;
 
           if (json.status.code === 1001) {
             message.success(msg);
@@ -45,85 +27,33 @@ class LoginForm extends React.Component {
     });
   };
 
-  handleSleep = () => {
-    const self = this;
-    let sleep = this.state.sleep;
-
-    if (sleep <= 15 && sleep !== 0) {
-      setTimeout(() => {
-        self.setState({
-          disabled: true,
-          getCodeBtnText: '请等待' + sleep + 's',
-          sleep: --sleep
-        });
-        self.handleSleep();
-      }, 1000);
-    }
-
-    if (sleep === 0) {
-      self.setState({
-        disabled: false,
-        getCodeBtnText: '获取验证码',
-        sleep: 15
-      });
-    }
-
-  }
-
-  handleGetCheck = () => {
-
-    const self = this;
-    const phone = this.props.form.getFieldValue('phone');
-    const params = {
-      phone: phone
-    };
-
-    axios.get('/api/admin/user/getCheckCode', { params: params }).then(res => {
-      const json = res.data;
-      const msg = json.result.msg;
-
-      if (json.status.code === 1001) {
-
-        self.setState({
-          disabled: true
-        });
-        message.success(msg);
-        self.handleSleep();
-      } else {
-        message.error(msg);
-      }
-    });
-  };
-
   render() {
     const { getFieldDecorator } = this.props.form;
-    const { disabled, getCodeBtnText } = this.state;
 
     return (
       <Form onSubmit={this.handleSubmit} className={styles.login_form}>
-        <Logo />
+        <p style={{ textAlign: "center" }}>管理台</p>
         <FormItem>
           <Row style={{ height: 45 }}>
-            {getFieldDecorator('phone', {
-              rules: [{
-                required: true,
-                message: '请输入手机号'
-              }]
-            })(<Input prefix={<Icon type="user" />} size="large" type="number" placeholder="手机号" />)}
+            <Col>
+              {getFieldDecorator('userName', {
+                rules: [{
+                  required: true,
+                  message: '请输入账号'
+                }]
+              })(<Input prefix={<Icon type="user" />} size="large" type="text" placeholder="账号" />)}
+            </Col>
           </Row>
         </FormItem>
         <FormItem>
           <Row style={{ height: 45 }}>
-            <Col span={16}>
-              {getFieldDecorator('code', {
+            <Col>
+              {getFieldDecorator('passWord', {
                 rules: [{
                   required: true,
-                  message: '验证码不能为空!'
-                }],
-              })(<Input prefix={<Icon type="lock" />} size="large" type="number" placeholder="验证码" />)}
-            </Col>
-            <Col span={8} style={{ textAlign: 'right' }}>
-              <Button size="large" onClick={this.handleGetCheck} disabled={disabled}>{getCodeBtnText}</Button>
+                  message: '请输入密码'
+                }]
+              })(<Input prefix={<Icon type="lock" />} size="large" type="password" placeholder="密码" />)}
             </Col>
           </Row>
         </FormItem>
@@ -135,14 +65,11 @@ class LoginForm extends React.Component {
   }
 }
 
-LoginForm.propTypes = {
-};
-
-const WrappedLoginForm = Form.create()(LoginForm);
+const WrappedLogin = Form.create()(Login);
 
 function IndexPage() {
   return (
-    <WrappedLoginForm></WrappedLoginForm>
+    <WrappedLogin />
   );
 }
 
