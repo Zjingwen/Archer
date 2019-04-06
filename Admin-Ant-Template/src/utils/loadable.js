21bb877be6641b98c0b79396c1d330876353e97e
静文（jingwen） <jingwen@xiaoshijie.com>
2019-04-07 04:00:58 +0800

    release: 调整loadable的loading样式

--- Admin-Ant-Template/src/utils/loadable.js
+++ Admin-Ant-Template/src/utils/loadable.js
@@ -1,12 +1,24 @@
 import Loadable from 'react-loadable';
 
+function Loading (){
+  const STYLE = {
+    width: '100%',
+    height: '100%',
+    textAlign: 'center',
+  };
+
+  return <div style={STYLE}>
+    ...loading...
+  </div>;
+}
+
 export default function(ipt){
   return Loadable({
     loader: () => ipt,
-    loading(){
-      return <div>...loading</div>;
+    loading: ()=> {
+      return <Loading />;
     },
-    delay: 3000,
+    delay: 30000,
   });
 }
 
